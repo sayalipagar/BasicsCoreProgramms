cc8dfa623f3bd7ad8965cf816cef84fe169f99f5
sayalipagar <pagarsayali565@gmail.com>
2022-08-25 17:44:02 +0530

    Basic Core Programms

---
+++ CoreProgramms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreProgramms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programms");
+        }
+    }
+}
