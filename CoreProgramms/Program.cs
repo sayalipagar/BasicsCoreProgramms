893290842de27d33dd2319518cd87178e57205ee
sayalipagar <pagarsayali565@gmail.com>
2022-08-26 11:00:13 +0530

    [sayali]Add:VowelOrConstant

--- CoreProgramms/Program.cs
+++ CoreProgramms/Program.cs
@@ -6,10 +6,10 @@
         {
             while (true)
             {
-                Console.WriteLine("\nselect program\n 1.FlipCoin\n 2.SwapTwoNo\n 3.EvenOdd");
+                Console.WriteLine("\n select program\n 1.FlipCoin\n 2.SwapTwoNo\n 3.EvenOdd\n 4.Vowel OR Constant");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
-                {
+                { 
                     case 1:
                         FlipCoin flipCoin = new FlipCoin();
                         flipCoin.ReadInput();
@@ -22,12 +22,15 @@
                         EvenOdd evenOdd = new EvenOdd();
                         evenOdd.Findno();
                         break;
-
-
+                    case 4:
+                        VowelConsonant vowelConsonant=new VowelConsonant();
+                        vowelConsonant.VowelOrConsonant();
                         
+  break;
 
 
-                      
+
+
 
 
                 }
---
+++ CoreProgramms/VowelConstant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgramms
+{
+    public  class VowelConsonant
+    {
+        public void VowelOrConsonant()
+        {
+            Console.WriteLine("Enter any character: ");
+            char c = Convert.ToChar(Console.ReadLine());
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            {
+                Console.WriteLine("vowel");
+
+            }
+            else
+            {
+                Console.WriteLine("consonant");
+
+            }
+
+        }
+
+    }
+}
+
